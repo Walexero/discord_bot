70107563d966ba88de03758452b364c7029503b3
Walexero <ayanladeolawale@gmail.com>
2022-10-18 15:27:37 +0000

    Added product query

--- dcrd2.py
+++ dcrd2.py
@@ -58,8 +58,8 @@ def _get_subscription_info():
     def _query_api(input_query):
         wcapi = API(
             url="https://learn-league.com/",
-            consumer_key= "ck_a482ff0296b8b4a56b74ac4dc0cffbcd9a3d7ed7",
-            consumer_secret= "cs_f2e6b2f73ce68d50dd36bcd10b64d0eea2aa1c3f",
+            consumer_key= os.getenv("PUB_KEY"),
+            consumer_secret= os.getenv("PRIV_KEY"),
             wp_api=True,
             verify_ssl = True,
             version="wc/v3",
@@ -105,7 +105,7 @@ def _get_subscription_info():
                 print(subscriber_order["product_id"])
 
         def get_order_payment(order):
-            
+
 
         if subscriber:
             order_lookup = _query_api("orders")
